4be4ec73c3af0814c699fc954f70db9c9ed47310
[okoro progress] <[progressewokoro@gmail.com]>
2023-04-28 15:06:43 +0100

    commited math

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        //TO DO
+
+        for(int i = 0; 1 < args.length; i++){
+            System.out.println(args[1]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+
+    float multiply (int numberOne, int numberTwo){
+        return  numberOne * numberTwo;
+    }
+
+    float divide (int numberOne, int numberTwo){
+        return  numberOne / numberTwo;
+    }
+
+    float powTwo (int number){
+        return  number  * number;
+    }
+}
